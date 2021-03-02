d769a6452b237de925d053331f1b7132b4ff69f7
Stephen <stephen.germain@free.Fr>
2021-03-02 18:15:38 +0100

    add update profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,19 +2,58 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\EditProfilFormType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfilController extends AbstractController
 {
     /**
-     * @Route("/profil", name="profil")
+     * @Route("/user/profil", name="profil")
      */
     public function index(): Response
     {
-        return $this->render('user_space/profil.html.twig', [
-            'controller_name' => 'ProfilController',
+        return $this->render('user_space/profil.html.twig');
+    }
+
+    /**
+     * @Route("/user/profil_upadate", name="profil_update")
+     */
+    public function profilUpdate(Request $request)
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfilFormType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            if($form->isValid()){
+                $data = $form->getData();
+                // échapper les caractères spéciaux 
+                $user->setName(strip_tags($data->getName()));
+
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($user);
+                $manager->flush();
+                $this->addFlash(
+                    'success',
+                    'Le profil à été modifiée'
+                );
+            }
+            else{
+                $this->addFlash(
+                    'danger',
+                    'Une erreur est survenue'
+                );
+            }
+
+            return $this->redirectToRoute('profil');
+        }
+
+
+        return $this->render('user_space/profilEdit.html.twig', [
+            'formUser' => $form->createView(),
         ]);
     }
 }
